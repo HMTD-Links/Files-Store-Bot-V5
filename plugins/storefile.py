8e0b979bd058fce01bcc44202d66deafaa213db4
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-07-25 16:31:09 +0530

    Update storefile.py

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -41,13 +41,13 @@ async def storefile(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/{bot.username}?start={base64_string}"
+    url = f"https://telegram.me/HMTD_Files_Store_V5_Bot?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
     # making buttons
     buttons = [[
-        InlineKeyboardButton(text="Open Url 🔗", url=url),
+        InlineKeyboardButton(text="Open URL 🔗", url=url),
         InlineKeyboardButton(text="Share Link 👤", url=share_url)
         ],[
         InlineKeyboardButton(text="Delete 🗑", callback_data=f"delete+{msg.message_id}")
@@ -96,13 +96,13 @@ async def storefile_channel(c, m):
     # creating urls
     bot = await c.get_me()
     base64_string = await encode_string(f"{m.chat.id}_{msg.message_id}")
-    url = f"https://t.me/{bot.username}?start={base64_string}"
+    url = f"https://telegram.me/HMTD_Files_Store_V5_Bot?start={base64_string}"
     txt = urllib.parse.quote(text.replace('--', ''))
     share_url = f"tg://share?url={txt}File%20Link%20👉%20{url}"
 
     # making buttons
     buttons = [[
-        InlineKeyboardButton(text="Open Url 🔗", url=url),
+        InlineKeyboardButton(text="Open URL 🔗", url=url),
         InlineKeyboardButton(text="Share Link 👤", url=share_url)
     ]]
 
